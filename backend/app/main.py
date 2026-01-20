6051ea7d8703326d52a2414b71310dc573f7679f
devmandal98 <mandaldev387@gamil.com>
2026-01-20 16:39:31 +0530

    changes in files for debugging some errors

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://movie-recommendation-ai-2.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
